024c20b55584aafa1f4f26a86e6cd6d06c564ae0
ntua-el20082 <el20082@mail.ntua.gr>
2024-01-26 12:53:56 +0200

    fixed some errors

--- back-end/controllers/userRegister.js
+++ back-end/controllers/userRegister.js
@@ -1,41 +1,37 @@
-const { create } = require("../middlewares/userRegister");
-
+const { pool } = require('../utils/database');
 const { genSaltSync, hashSync } = require("bcrypt");
 
-exports.createUser = (req, res) => {
-        const body = req.body;
-        const salt = genSaltSync(10);
-        body.password = hashSync(body.password, salt);
-        create(body, (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                })
+exports.createUser = (data, callBack) => {
+    pool.query(
+        `insert into user(username, password) values(?,?)`,
+        [
+            data.username,
+            data.password
+        ],
+        (error, results, fields) => {
+            if (error) {
+                return callBack(error)
             }
-            return res.status(200).json({
-                success: 1,
-                data: results
-            });
-        });
-}
+            return callBack(null, results)
+        }
+    );
+};
 
-exports.createUser = (req, res) => {
-        const body = req.body;
-        const salt = genSaltSync(10);
-        body.password = hashSync(body.password, salt);
-        create(body, (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                })
-            }
-            return res.status(200).json({
-                success: 1,
-                data: results
-            });
+exports.createUserHandler = (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    exports.createUser(body, (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                success: 0,
+                message: "Database connection error"
+            })
+        }
+        return res.status(200).json({
+            success: 1,
+            data: results
         });
-}
+    });
+};
