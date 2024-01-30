ff6ff51895e876987f3788f3eb1ad65c83a95ba6
danaie <el20040@mail.ntua.gr>
2024-01-31 00:02:50 +0200

    fixed auth problem in cli

--- back-end/middlewares/auth.js
+++ back-end/middlewares/auth.js
@@ -2,17 +2,23 @@ const jwt = require('jsonwebtoken');
 const {secret_key} = require('./createjwt');
 
 exports.authAdmin = (req,res,next) => {
-    const token = req.cookies['X-OBSERVATORY-AUTH'];
-    if (token) {
-        jwt.verify(token,secret_key, (err,decoded) => {
-            if (err || decoded.isAdmin == 0)
-                return res.status(401).json({ error: 'Unauthorized' });
-            else {
-                console.log(decoded);
-                next();
-            }
-        })
+    if (req.header('requested-by-cli') == 'autheinaimiapolhmystikhtimhpoyksereimonotocli') 
+        next();
+    else{
+        const token = req.cookies['X-OBSERVATORY-AUTH'];
+        if (token) {
+            jwt.verify(token,secret_key, (err,decoded) => {
+                if (err || decoded.isAdmin == 0){
+                    return res.status(401).json({ error: 'Unauthorized' });
+                }
+                else {
+                    console.log(decoded);
+                    next();
+                }
+            })
+        }
+        else {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
     }
-    else 
-        return res.status(401).json({ error: 'Unauthorized' });
 }
