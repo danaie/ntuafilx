7d8b898f7dac8862962519a15d8a3422e6c0c9b5
ntua-el20082 <147534112+ntua-el20082@users.noreply.github.com>
2024-02-02 14:31:54 +0200

    Delete front-end/.next directory
    sorry again

--- front-end/.next/static/development/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/homepage":["static\u002Fchunks\u002Fpages\u002Fhomepage.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],"/results":["static\u002Fchunks\u002Fpages\u002Fresults.js"],"/signup":["static\u002Fchunks\u002Fpages\u002Fsignup.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhomepage","\u002Flogin","\u002Fresults","\u002Fsignup"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/homepage":["static\u002Fchunks\u002Fpages\u002Fhomepage.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhomepage"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 0feffd2902e506454e4b9279436737e550b4027b
