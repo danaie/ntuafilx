ff5e51cc230a769a7097cf3457e13b20df47224b
ntua-el20082 <el20082@mail.ntua.gr>
2024-02-08 00:24:47 +0200

    user endpoints fixed

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -1,7 +1,7 @@
 const { pool } = require('../utils/database');
 
 exports.getTitles = (req, res, next) => {
-    const q = 'SELECT originalTitle FROM title';
+    const q = 'SELECT titleID, originalTitle FROM title';
     pool.query(q, (error, results, fields) => {
         if (error) {
             console.error('Error executing SELECT query:', error);
@@ -17,95 +17,152 @@ exports.getTitles = (req, res, next) => {
     });
 };
 
+const getTitleObjectById = (titleID) => {
+    return new Promise((resolve, reject) => {
+        const q1 = 'SELECT titleID, titleType, originalTitle, image, startYear, endYear, genres FROM title WHERE titleID = ?';
+        const q2 = 'SELECT titleID, title, region FROM titleAKA WHERE titleID = ?';
+        const q3 = 'SELECT * FROM principalsList WHERE titleID = ?';
+        const q4 = 'SELECT * FROM ratings WHERE titleID = ?';
+
+        pool.getConnection((err, connection) => {
+            if (err) {
+                console.log(err);
+                connection.release();
+                reject({ error: err });
+            } else {
+                connection.query(q1, [titleID], (error, result1) => {
+                    if (error) {
+                        connection.release();
+                        reject({ error: 'Error executing query 1' });
+                    }
+                    if (result1.length === 0) {
+                        connection.release();
+                        resolve({ message: 'Movies not found' });
+                    }
+
+                    connection.query(q2, [titleID], (error, result2) => {
+                        if (error) {
+                            connection.release();
+                            reject({ error: 'Error executing query 2' });
+                        }
+
+                        connection.query(q3, [titleID], (error, result3) => {
+                            if (error) {
+                                connection.release();
+                                reject({ error: 'Error executing query 3' });
+                            }
+
+                            connection.query(q4, [titleID], (error, result4) => {
+                                connection.release();
+                                if (error) {
+                                    reject({ error: 'Error executing query 4' });
+                                }
+
+                                resolve({
+                                    TitleObject: {
+                                        titleInfo: result1,
+                                        titleAKAlist: result2,
+                                        titlePrincipalsList: result3,
+                                        ratings: result4
+                                    }
+                                });
+                            });
+                        });
+                    });
+                });
+            }
+        });
+    });
+};
+
+const getnameObjectById = (nameID) => {
+    return new Promise((resolve, reject) => {
+        const query = 'SELECT n.basicsID, n.primaryName, n.image, n.birthYear, n.deathYear, n.primaryProfession, p.titleID, p.category FROM nameBasics n INNER JOIN principals p ON n.basicsID = p.basicsID WHERE n.basicsID = ?';
+
+        pool.query(query, [nameID], (error, results, fields) => {
+            if (error) {
+                reject({ error: 'Error executing query' });
+            } else {
+                const personData = {
+                    basicsID: null,
+                    primaryName: null,
+                    image: null,
+                    birthYear: null,
+                    deathYear: null,
+                    primaryProfession: null,
+                    titles: []
+                };
+
+                if (results.length > 0) {
+                    personData.basicsID = results[0].basicsID;
+                    personData.primaryName = results[0].primaryName;
+                    personData.image = results[0].image;
+                    personData.birthYear = results[0].birthYear;
+                    personData.deathYear = results[0].deathYear;
+                    personData.primaryProfession = results[0].primaryProfession;
+
+                    results.forEach((row) => {
+                        personData.titles.push({
+                            titleID: row.titleID,
+                            category: row.category
+                        });
+                    });
+                }
+
+                resolve({ personData });
+            }
+        });
+    });
+};
+
 exports.getTitleById = (req, res) => {
     const titleID = req.params.titleID;
-    const q1 = 'SELECT titleID, titleType, originalTitle, image, startYear, endYear, genres FROM title WHERE titleID = ?';
-    const q2 = 'SELECT titleID, title, region FROM titleAKA WHERE titleID = ?';
-    const q3 = 'SELECT * FROM principalsList WHERE titleID = ?';
-    const q4 = 'SELECT * FROM ratings WHERE titleID = ?';
-
-    const titleInfoPromise = new Promise((resolve, reject) => {
-        pool.query(q1, [titleID], (error, results) => {
-            if (error) {
-                console.error('Error executing SELECT query:', error);
-                reject('Database error');
-            } else if (results.length === 0) {
-                reject(`Movie not found with ${titleID}`);
-            } else {
-                resolve(results[0]);
-            }
-        });
+    getTitleObjectById(titleID)
+    .then((titleObject) => {
+        if (titleObject.status === 404) {
+            return res.status(404).json(titleObject);
+        }
+        return res.status(200).json(titleObject);
+    })
+    .catch((error) => {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal Server Error' });
     });
-
-    const titleAKAPromise = new Promise((resolve, reject) => {
-        pool.query(q2, [titleID], (error, results) => {
-            if (error) {
-                console.error('Error executing SELECT query:', error);
-                reject('Database error');
-            } else {
-                resolve(results);
-            }
-        });
-    });
-
-    const titlePrincipalsListPromise = new Promise((resolve, reject) => {
-        pool.query(q3, [titleID], (error, results) => {
-            if (error) {
-                console.error('Error executing SELECT query:', error);
-                reject('Database error');
-            } else {
-                resolve(results);
-            }
-        });
-    });
-
-    const ratingsPromise = new Promise((resolve, reject) => {
-        pool.query(q4, [titleID], (error, results) => {
-            if (error) {
-                console.error('Error executing SELECT query:', error);
-                reject('Database error');
-            } else {
-                resolve(results[0]);
-            }
-        });
-    });
-
-    Promise.all([titleInfoPromise, titleAKAPromise, titlePrincipalsListPromise, ratingsPromise])
-        .then(([titleInfo, titleAKAlist, titlePrincipalsList, ratings]) => {
-            return res.status(200).json({
-                success: 1,
-                data: { titleInfo, titleAKAlist, titlePrincipalsList, ratings }
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                success: 0,
-                message: error
-            });
-        });
 };
 
 exports.getSearchTitle = (req, res) => {
     const { titlePart } = req.query;
-    const q2 = 'SELECT originalTitle FROM title WHERE originalTitle LIKE ?';
-    pool.query(q2, [`%${titlePart}%`], (error, results, fields) => {
+    const q = 'SELECT titleID FROM title WHERE originalTitle LIKE ?';
+    pool.query(q, [`%${titlePart}%`], (error, searchresults, fields) => {
         if (error) {
-            console.error('Error executing SELECT query:', error);
+            console.error('Error executing query', error);
             return res.status(500).json({
                 success: 0,
                 message: 'Database error'
             });
         }
-        if (results.length === 0) {
+        if (searchresults.length === 0) {
             return res.status(404).json({
                 success: 0,
-                message: 'Movies not found'
+                message: 'Movie not found'
             });
         }
-        return res.status(200).json({
-            success: 1,
-            data: results
+        const promises = searchresults.map((result) => {
+            return getTitleObjectById(result.titleID)
+                .catch((error) => {
+                    console.error(error);
+                    return { error: 'Error getting title object' };
+                });
         });
+        Promise.all(promises)
+            .then((titleObjects) => {
+                //console.log("Search Results:", titleObjects);
+                return res.status(200).json(titleObjects);
+            })
+            .catch((error) => {
+                console.error(error);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            });
     });
 };
 
@@ -114,27 +171,40 @@ exports.getbyGendre =  (req, res, next) => {
     const { minrating } = req.query;
     const { yrFrom } = req.query;
     const { yrTo } = req.query;
-    q = "SELECT originalTitle, startYear FROM (title INNER JOIN ratings USING (titleID)) WHERE genres LIKE ? AND averageRating >= ?";
+    q = "SELECT titleID FROM (title INNER JOIN ratings USING (titleID)) WHERE genres LIKE ? AND averageRating >= ?";
     if (yrFrom != null && yrTo != null) { q = q + "AND startYear BETWEEN ? AND ?" }
-    pool.query(q, [`%${qgenre}%`, minrating, yrFrom, yrTo], (error, results) => {
+    pool.query(q, [`%${qgenre}%`, minrating, yrFrom, yrTo], (error, searchresults, fields) => {
         if (error) {
-            console.error('Error executing SELECT query:', error);
+            console.error('Error executing query', error);
             return res.status(500).json({
                 success: 0,
                 message: 'Database error'
             });
         }
-        if (results.length === 0) {
+        if (searchresults.length === 0) {
             return res.status(404).json({
                 success: 0,
                 message: 'Movie not found'
             });
         }
-        return res.status(200).json({
-            success: 1,
-            data: results
-        });
+        const promises = searchresults.map((result) => {
+            return getTitleObjectById(result.titleID)
+                .catch((error) => {
+                    console.error(error);
+                    return { error: 'Error getting title object' };
+                });
+            });
+            Promise.all(promises)
+            .then((titleObjects) => {
+                //console.log("Search Results:", titleObjects);
+                return res.status(200).json(titleObjects);
+            })
+            .catch((error) => {
+                console.error(error);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            });
     });
+
 }
 
 exports.getNames =  (req, res, next) => {
@@ -156,68 +226,64 @@ exports.getNames =  (req, res, next) => {
 
 exports.getNameById = (req, res) => {
     const nameID = req.params.nameID; 
-    const q1 = 'SELECT basicsID, primaryName, image, birthYear, deathYear, primaryProfession FROM nameBasics WHERE basicsID = ?';
-    const q2 = "SELECT basicsID, category, SUBSTRING_INDEX(SUBSTRING_INDEX(knowForTitles, ',', n.digit + 1), ',', -1) AS knowForTitle FROM (nameBasics INNER JOIN principals USING (basicsID)) JOIN (SELECT 0 AS digit UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3) AS n ON LENGTH(REPLACE(knowForTitles, ',' , '')) <= LENGTH(knowForTitles) - n.digit WHERE basicsID = ?";
-    
-    const nameInfoPromise = new Promise((resolve, reject) => {
-        pool.query(q1, [nameID], (error, results) => {
-            if (error) {
-                console.error('Error executing SELECT query:', error);
-                reject('Database error');
-            } else if (results.length === 0) {
-                reject(`Name not found with ${nameID}`);
-            } else {
-                resolve(results[0]);
-            }
-        });
-    });
-    const knownForPromise = new Promise((resolve, reject) => {
-        pool.query(q2, [nameID], (error, results) => {
-            if (error) {
-                console.error('Error executing SELECT query:', error);
-                reject('Database error');
-            } else if (results.length === 0) {
-                reject(`Movie not found with ${nameID}`);
-            } else {
-                resolve(results);
-            }
-        });
-    });
-    Promise.all([nameInfoPromise, knownForPromise,])
-        .then(([nameInfo, knownFor]) => {
-            return res.status(200).json({
-                success: 1,
-                data: { nameInfo, knownFor }
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                success: 0,
-                message: error
-            });
-        });
+    const query = 'SELECT n.basicsID, n.primaryName, n.image, n.birthYear, n.deathYear, n.primaryProfession, p.titleID, p.category FROM nameBasics n inner join principals p on n.basicsID = p.basicsID WHERE n.basicsID = ?'
+    pool.query(query,nameID, (error, results, fields) => {
+        if (error) throw error;
+        const personData = results.map(row => ({
+            basicsID: row.basicsID,
+            primaryName: row.primaryName,
+            image: row.image,
+            birthYear: row.birthYear,
+            deathYear: row.deathYear,
+            primaryProfession: row.primaryProfession,
+            titles: results.map(title => ({
+                titleID: title.titleID,
+                category: title.category
+          }))
+        }));
+        res.status(200).json({success: 1,data:personData[0]})
+      });    
 };
 
-exports.getSearchName =  (req, res, next) => {
+exports.getSearchName = (req, res) => {
     const { namePart } = req.query;
-    const q2 = 'SELECT primaryName FROM nameBasics WHERE primaryName LIKE ?';
-    pool.query(q2, [`%${namePart}%`], (error, results, fields) => {
+    const q = 'SELECT basicsID FROM nameBasics WHERE primaryName LIKE ?';
+
+    pool.query(q, [`%${namePart}%`], (error, searchresults, fields) => {
         if (error) {
-            console.error('Error executing SELECT query:', error);
+            console.error('Error executing query', error);
             return res.status(500).json({
                 success: 0,
                 message: 'Database error'
             });
         }
-        if (results.length === 0) {
+
+        if (searchresults.length === 0) {
             return res.status(404).json({
                 success: 0,
                 message: 'Names not found'
             });
         }
-        return res.status(200).json({
-            success: 1,
-            data: results
+
+        const promises = searchresults.map((result) => {
+            return getnameObjectById(result.basicsID)  // Ensure basicsID matches the column name
+                .catch((error) => {
+                    console.error(error);
+                    return { error: 'Error getting name object' };
+                });
         });
+
+        Promise.all(promises)
+            .then((nameObjects) => {
+                console.log("Search Results:", nameObjects);
+                // If you want to filter out objects with errors, you can use:
+                // const validNameObjects = nameObjects.filter(obj => !obj.error);
+                return res.status(200).json(nameObjects);
+            })
+            .catch((error) => {
+                console.error(error);
+                return res.status(500).json({ error: 'Internal Server Error' });
+            });
     });
-}
+};
+
